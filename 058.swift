a1594fd5a98651a306a13a36d7ec953c97efd7b4
Suruikame <shiso0913@gmail.com>
2021-06-05 11:20:14 +0900

    modified

--- 058.swift
+++ 058.swift
@@ -4,10 +4,10 @@ func readInts() -> [Int]{
 
 func main(){
     let inputs = readInts()
-    let (N, K) = (inputs[0], inputs[1])
-    var ls = Array(repeating: [Int](0..<100_000), count: 71)
+    var (N, K) = (inputs[0], inputs[1])
+    var ls = [Int](0..<100_000)
     //各桁の和を求める
-    func digitsSum(_ n: Int) -> Int {
+    func digitSum(_ n: Int) -> Int {
         var m = n
         var ret = 0
         while m > 0 {
@@ -18,19 +18,15 @@ func main(){
     }
     //1回遷移後の数字
     for i in 0..<100_000 {
-        ls[0][i] = (i + digitsSum(i)) % 100_000
-    }
-    for i in 0..<70 {
-        //2^i回遷移後の数字
-        for j in 0..<100_000 {
-            ls[i+1][j] = ls[i][ls[i][j]]
-        }
+        ls[i] = (i + digitSum(i)) % 100_000
     }
     var ans = N
-    for i in 0..<63 {
-        if ((K>>i) & 1) == 1 {
-            ans = ls[i][ans]
+    while K > 0 {
+        if K&1 == 1 {
+            ans = ls[ans]
         }
+        ls = ls.map {ls[$0]}
+        K >>= 1
     }
     print(ans)
 }
